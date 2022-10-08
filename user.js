3d1dcf22a10f8a16a7a17e1c5497b98b2da97558
Akib <akibmahdi1@gmail.com>
2022-10-08 16:15:42 -0400

    init

--- user.js
+++ user.js
@@ -5,84 +5,49 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
-
+/*PUT YOUR OWN KEY HERE - THIS MIGHT NOT WORK
+SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
 const apiKey = "aaa928d1b15a7fc8c1f242d4b2d72cdc";
-form.addEventListener("submit", e => {  //addeventlistener
-    e.preventDefault();
-    const inputVal = input.value;
 
-//ajax request//
+form.addEventListener("submit", e =>{
+  e.preventDefault();
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const inputVal = input.value;
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-        fetch(url) //conversion
-        .then(response => response.json())
-        .then(data => {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://openweathermap.org/img/wn/${
+        weather[0]["icon"]
+      }@2x.png`;
 
-const { main, name, sys, weathers } = data;
-const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city‼️";
+    });
 
-const li = document.createElement("li");
-li.classList.add("city");
-const markup = `
-  <h2 class="city-name" data-name="${name},${sys.country}">
-    <span>${name}</span>
-    <sup>${sys.country}</sup>
-  </h2>
-  <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
-  </div>
-  <figure>
-    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-    <figcaption>${weathers[0]["description"]}</figcaption>
-  </figure>
-`;
-li.innerHTML = markup;
-list.appendChild(li);
-})
-.catch(() => {
-    msg.textContent = "Please search for a valid city 🙃";
-});
-//reset field//
-msg.textContent = "";
-form.requestFullscreen();
-input.focus();
-
-//potentially change icon pathway here//
-
-// here you will check if there is already a city//
-const listItems = list.querySelectorAll(".ajax-section .city");
-const listItemsArray = Array.from(listItems);
-
-if (listItemsArray.length > 0) {
-  //2
-  const filteredArray = listItemsArray.filter(el => {
-    let content = "";
-    //athens,gr
-    if (inputVal.includes(",")) {
-      //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-      if (inputVal.split(",")[1].length > 2) {
-        inputVal = inputVal.split(",")[0];
-        content = el.querySelector(".city-name span").textContent.toLowerCase();
-      } else {
-        content = el.querySelector(".city-name").dataset.name.toLowerCase();
-      }
-    } else {
-      //athens
-      content = el.querySelector(".city-name span").textContent.toLowerCase();
-    }
-    return content == inputVal.toLowerCase();
-  });
-
-  //3
-  if (filteredArray.length > 0) {
-    msg.textContent = `You already know the weather for ${
-      filteredArray[0].querySelector(".city-name span").textContent
-    } ...otherwise be more specific by providing the country code as well 😉`;
-    form.reset();
-    input.focus();
-    return;
-  }
-}
+  msg.textContent = "";
+  form.reset();
+  input.focus();
 });
 
 //Again during the submit handler, before making an AJAX request, we check to see whether the unordered list is empty or not. If it isn’t empty, that means at least one successful AJAX request has already been executed. 
